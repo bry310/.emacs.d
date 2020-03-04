cac64df54856f61170a14a34c85705cd925294ed
bry310 <bryanbaker310@gmail.com>
2020-03-03 19:27:14 -0500

    a flycheck to init.el

--- init.el
+++ init.el
@@ -1,15 +1,23 @@
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+'("melpa" . "https://melpa.org/packages/"));; stuff automatically added
+;;
 
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
+(package-initialize)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (try use-package))))
+ '(package-selected-packages
+   (quote
+    (flycheck elpy org-bullets which-key try use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -17,43 +25,55 @@
  ;; If there is more than one, they won't work right.
  )
 
-
 ;; My Stuff
 
 
 (setq inhibit-startup-message t)
+(tool-bar-mode -1)
 
- (setq url-proxy-services
-       '(("no_proxy" . "^\\(localhost\\|10.*\\)")
-         ("http" . "196.3.0.55:8080")
-         ("https" . "196.3.0.55:8080")))
+;;UWI-Mona proxy
+(setq url-proxy-services
+      '(("no_proxy" . "^\\(localhost\\|10.*\\)")
+        ("http" . "196.3.0.55:8080")
+        ("https" . "196.3.0.55:8080")))
+
 
 
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/"))
-
-(package-initialize)
-
-;; Bootstrap 'use-package'
-(unless (packege-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
+'("melpa" . "https://melpa.org/packages/"))
 
 
+;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package)
+(package-refresh-contents)
+(package-install 'use-package))
 
 (use-package try
-  :ensure t)
+	:ensure t)
 
 (use-package which-key
-  :ensure t
-  :config (which-key-mode))
+	:ensure t 
+	:config
+	(which-key-mode))
 
- Org-mode stuff
+
+;; Org-mode stuff
 (use-package org-bullets
   :ensure t
   :config
   (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+
+;;Python stuff
+
+(use-package elpy
+  :ensure t
+  :init
+  (elpy-enable))
+
+(use-package flycheck
+  :ensure t
+  )
 
 
